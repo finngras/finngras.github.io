a9d2849c5b3d17e8039bc703b11e3b8e8c147acb
finngras <166633988+finngras@users.noreply.github.com>
2024-06-08 23:50:58 +0200

    Full Jekyll migration

---
+++ assets/js/top-button.js
@@ -0,0 +1,12 @@
+//hides and reveals a back to top button
+let topButton = document.getElementById("top-button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
